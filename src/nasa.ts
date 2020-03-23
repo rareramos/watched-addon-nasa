36be17b537d52d40607babb4739b5756733e7a7b
maximilize <max@watched.com>
2020-03-23 12:38:31 +0100

    Migrated to cursor pagination

--- src/nasa.ts
+++ src/nasa.ts
@@ -1,6 +1,6 @@
-import { ChannelItem, DirectoryItem, DirectoryFeatures } from '@watchedcom/sdk';
-import fetch from 'node-fetch';
-import { parse as parseUrl, format as formatUrl } from 'url';
+import { ChannelItem, DirectoryItem, DirectoryFeatures } from "@watchedcom/sdk";
+import fetch from "node-fetch";
+import { parse as parseUrl, format as formatUrl } from "url";
 
 /*
 let locales = require('./locales.json');
@@ -8,7 +8,7 @@ locales = locales.map(item => ({ key: item.code, value: item.name }));
 */
 
 // const apiUrl = 'https://www.nasa.gov';
-const apiUrl = 'https://images-api.nasa.gov';
+const apiUrl = "https://images-api.nasa.gov";
 
 const logger = (...args) => {
   if (process.env.DEBUG) {
@@ -17,18 +17,24 @@ const logger = (...args) => {
 };
 
 class NasaApi {
-  async getVideos({ search = '', filter = {}, page = 1 }) {
-    return await this.get('search', {
+  async getVideos({ search = "", filter = {}, cursor }) {
+    const page = cursor === null ? 1 : <number>cursor;
+    return await this.get("search", {
       ...filter,
       q: search,
-      media_type: 'video',
+      media_type: "video",
       page,
     }).then(({ collection: data }) => {
       const items = Array.from(data.items || []).map<ChannelItem>((item: any) =>
         this.convertChannel(item)
       );
       return {
-        hasMore: Array.from(data.links || []).findIndex((it: any) => it.rel === 'next') >= 0,
+        nextCursor:
+          Array.from(data.links || []).findIndex(
+            (it: any) => it.rel === "next"
+          ) >= 0
+            ? page + 1
+            : null,
         items,
         features: {
           filter: [],
@@ -39,7 +45,7 @@ class NasaApi {
 
   async getVideo({ ids }) {
     const id = ids.id;
-    return await this.get('search', {
+    return await this.get("search", {
       nasa_id: id,
     }).then(({ collection: data }) => {
       const items = Array.from(data.items || []).map<ChannelItem>((item: any) =>
@@ -52,12 +58,12 @@ class NasaApi {
   convertChannel(item: any): ChannelItem {
     let { data, links } = item;
     data = data[0];
-    const preview = links.find(it => it.rel === 'preview');
-    const caption = links.find(it => it.rel === 'captions');
+    const preview = links.find((it) => it.rel === "preview");
+    const caption = links.find((it) => it.rel === "captions");
 
     const channel: ChannelItem = {
       id: data.nasa_id,
-      type: 'channel',
+      type: "channel",
       ids: { id: data.nasa_id },
       name: data.title,
       description: data.description,
@@ -71,8 +77,8 @@ class NasaApi {
       channel.sources?.push({
         id: data.nasa_id,
         name: data.title,
-        type: 'url',
-        url: String(preview.href).replace('~thumb.jpg', '~orig.mp4'),
+        type: "url",
+        url: String(preview.href).replace("~thumb.jpg", "~orig.mp4"),
       });
     }
     return channel;
@@ -135,7 +141,7 @@ class NasaApi {
   }
   */
 
-  async get(pathname = '', query = {}, options = {}) {
+  async get(pathname = "", query = {}, options = {}) {
     return this.api({ pathname, query }, options);
   }
 
@@ -144,7 +150,7 @@ class NasaApi {
       { pathname, query },
       {
         ...options,
-        method: 'post',
+        method: "post",
         body: data,
       }
     );
@@ -155,7 +161,7 @@ class NasaApi {
       { pathname, query },
       {
         ...options,
-        method: 'put',
+        method: "put",
         body: data,
       }
     );
@@ -166,7 +172,7 @@ class NasaApi {
       { pathname, query },
       {
         ...options,
-        method: 'delete',
+        method: "delete",
       }
     );
   }
@@ -178,16 +184,18 @@ class NasaApi {
       //'Content-Type': 'application/json',
       ...headers,
     };
-    if (body && typeof body === 'object') {
-      if (headers['Content-Type'] === 'application/json') {
+    if (body && typeof body === "object") {
+      if (headers["Content-Type"] === "application/json") {
         body = this.handleBodyAsJson(body);
-      } else if (headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      } else if (
+        headers["Content-Type"] === "application/x-www-form-urlencoded"
+      ) {
         body = this.handleBodyAsFormUrlencoded(body);
       }
     }
     let opts = { ...options, body, headers };
     const apiUrl = this.apiUrl(url);
-    logger('request', apiUrl, opts);
+    logger("request", apiUrl, opts);
     const res = await fetch(apiUrl, opts);
     return this.handleResponse(res);
   }
@@ -195,7 +203,7 @@ class NasaApi {
   apiUrl(url: any = {}) {
     let { pathname, query = {}, ...other } = url;
     let parsedApiUrl = parseUrl(apiUrl);
-    if (String(pathname).startsWith('http')) {
+    if (String(pathname).startsWith("http")) {
       parsedApiUrl = parseUrl(pathname);
       pathname = parsedApiUrl.pathname;
     }
@@ -208,8 +216,8 @@ class NasaApi {
   }
 
   async handleResponse(res) {
-    const contentType = res.headers.get('content-type') || 'text';
-    if (contentType.includes('json')) {
+    const contentType = res.headers.get("content-type") || "text";
+    if (contentType.includes("json")) {
       return this.handleResponseAsJson(res);
     }
     return this.handleResponseAsText(res);
@@ -223,9 +231,11 @@ class NasaApi {
     return Object.entries(body)
       .filter(([, value]) => value !== undefined)
       .map(([key, value]) =>
-        Array.isArray(value) ? value.map(item => `${key}=${item}`).join('&') : `${key}=${value}`
+        Array.isArray(value)
+          ? value.map((item) => `${key}=${item}`).join("&")
+          : `${key}=${value}`
       )
-      .join('&');
+      .join("&");
   }
 
   async handleResponseAsJson(res) {
@@ -238,7 +248,7 @@ class NasaApi {
       return null;
     }
     let data = await res.json();
-    data = typeof data === 'string' ? JSON.parse(data) : data;
+    data = typeof data === "string" ? JSON.parse(data) : data;
     //logger('response', res.url, res.status, res.headers.get('content-type'), data);
     return data;
   }
